dba3674f150c62d5907ec15b7c7d8af5b6da7b7d
Quinton Fults <quinton.fults.work@gmail.com>
2021-07-06 13:02:26 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import Card from './components/Card'
 import Form from './components/Form'
 
+const name = 'John Doe'
+const isSignedIn = true
+
+const users = [
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com'
+  },
+  {
+    name: 'Jane Doe',
+    email: 'janedoe@gmail.com'
+  },
+  {
+    name: 'Jack Doe',
+    email: 'jackdoe@gmail.com'
+  }
+]
+
 const App = () => {
   return (
     <>
-      <h1>Hello World!</h1>
-      <button>Click Me</button>
+      {
+        users.map((user, i) => (
+          <div key={i}>
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+          </div>
+        ))
+      }
+      {
+        [<button>1</button>, <button>2</button>, <button>3</button>]
+      }
+      {
+        (() => <span>some text</span>)()
+      }
+      <h1>You are {isSignedIn ? '' : 'not'} signed in</h1>
+      <h1>Hello {name}</h1>
+      <button>Click Me {6 * 7}</button>
       <Card />
       <Form />
     </>
