8d96d286d52bead191d0825f924153ce65f63d20
Quinton Fults <quinton.fults.work@gmail.com>
2021-07-06 11:22:23 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import './App.css'
+import Card from './components/Card'
+import Form from './components/Form'
 
 const App = () => {
   return (
     <>
       <h1>Hello World!</h1>
       <button>Click Me</button>
-      <div className='card text-white bg-primary mb-3 myCard'>
-        <div className='card-header'>Header</div>
-        <div className='card-body'>
-          <h5 className='card-title'>Primary card title</h5>
-          <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-      <form>
-        <div className='mb-3'>
-          <label htmlFor='exampleInputEmail1' className='form-label'>Email address</label>
-          <input type='email' className='form-control' id='exampleInputEmail1' aria-describedby='emailHelp' />
-          <div id='emailHelp' className='form-text'>We'll never share your email with anyone else.</div>
-        </div>
-        <div className='mb-3'>
-          <label htmlFor='exampleInputPassword1' className='form-label'>Password</label>
-          <input type='password' className='form-control' id='exampleInputPassword1' />
-        </div>
-        <div className='mb-3 form-check'>
-          <input type='checkbox' className='form-check-input' id='exampleCheck1' />
-          <label className='form-check-label' htmlFor='exampleCheck1'>Check me out</label>
-        </div>
-        <button type='submit' className='btn btn-primary'>Submit</button>
-      </form>
+      <Card />
+      <Form />
     </>
   )
 }
